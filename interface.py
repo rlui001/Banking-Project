397ad52305c074ba89fd250fa464d4853af3a753
Ronson Lui <ron@Ronsons-MacBook-Pro.local>
2021-12-22 02:58:44 -0800

    Add menu logic while loop placeholder

--- interface.py
+++ interface.py
@@ -1,9 +1,15 @@
-
+import sys
 
 def startMenu ():
     """
     Triggers the start menu for a user to use as the interface with the banking system.
-    User should enter the corresponding number, otherwise the system repeats the menu.
+    """
+    return """
+    Welcome to the Lui Banking System. Please select an option:  
+        1. New customer
+        2. Existing customer
+        3. Employee login
+        4. Exit
     """
     print ('-------------------------------------------------------------\n')
     print ('Welcome to the Lui Banking System. Please select an option:  \n')
@@ -32,4 +38,20 @@ def loginCustomer():
 if __name__ == "__main__":
     #db connection should be set up here
 
-    startMenu()
+    usr_input = input(startMenu())
+    while True:
+        if usr_input == '1':
+            print ('Do something with newCustomer()')
+            break
+        elif usr_input == '2':
+            print ('Do something with loginCustomer()')
+            break
+        elif usr_input == '3':
+            print ('Do something with employee')
+            break
+        elif usr_input == '4':
+            break
+        else:
+            usr_input = input('Please enter a valid input: ')
+    
+    print ('System log off.')
