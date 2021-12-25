b94896fad37a0262ce2a6aa82a910b083b0f52c4
rlui001 <rlui001@ucr.edu>
2021-12-24 20:16:52 -0800

    refactor code in newCustomer() and loginCustomer()

--- interface.py
+++ interface.py
@@ -32,19 +32,15 @@ def newCustomer(conn):
     val_c = (customer.ssn, customer.first, customer.last, customer.address)
     stmt_a = "INSERT INTO Account (cid, balance, rate, account_type, terminate) VALUES (%s, %s, %s, %s, %s)"
     val_a = (customer.ssn, 0, 0.0, 'Checking', False)
-    try:
-        #attempt to add record to Customer table
-        conn.execute(stmt_c, val_c)
-        try:
-            #attempt to add record to Account table
-            conn.execute(stmt_a, val_a)
-        except exc.SQLAlchemyError as e:
-            print (f'failed due to: {e}')
-        print ('Registered successfully. A checking account has also been created automatically.\n')
-        return True
 
-    except exc.SQLAlchemyError as e:
-        print (f"Failed due to: {e}")
+    #attempt to add record to Customer table
+    execute_db(stmt_c, val_c, conn)
+
+    #attempt to add record to Account table
+    execute_db(stmt_a, val_a, conn)
+
+    print ('Registered successfully. A checking account has also been created automatically.\n')
+    return True
 
 def loginCustomer(conn):
     """
@@ -54,16 +50,10 @@ def loginCustomer(conn):
     ssn = input('Please enter your SSN to login: ')
     stmt = "SELECT * FROM Customer WHERE ssn = %s"
 
-    try:
-        result = conn.execute(stmt, ssn).first()
-        if not result:
-            raise ValueError("No customer with that SSN found.")
-        return Customer(str(result[0]), result[1], result[2], result[3])
-
-    except exc.SQLAlchemyError as e:
-        print (e)
-
-
+    result = execute_db(stmt, ssn, conn, 'first')
+    if not result:
+        raise ValueError("No customer with that SSN found.")
+    return Customer(str(result[0]), result[1], result[2], result[3])
 
 
 
